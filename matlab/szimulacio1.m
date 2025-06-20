c24a7b35a7e1adacddb123c3b14c61fa698a758c
MagyariCsaba <99736187+MagyariCsaba@users.noreply.github.com>
2025-06-20 16:44:34 +0300

    Update szimulacio1.m

--- matlab/szimulacio1.m
+++ matlab/szimulacio1.m
@@ -1,22 +1,24 @@
-mqClient = mqttclient("tcp://192.168.18.241", ClientID="myClient", Port=1883);
+mqClient = mqttclient("tcp://192.168.82.241", ClientID="myClient", Port=1883);
 mqClient.Connected
 topicToWrite = "eesTopic";
 
 
 lla0 = [46.5382, 24.5623, 320];  
 
+
 lastGPS = lla0;
 
+
 s = drivingScenario('GeoReference',lla0);      
-v = vehicle(s);                                
+v = vehicle(s);                               
 
 waypoints = [-11 -0.25 0;                      
              -1 -0.25 0;
              -0.6 -0.4 0;
              -0.6 -9.3 0];
 
-speed = [1.5;0.5;0.5;1.5];                     
-smoothTrajectory(v,waypoints,speed);         
+speed = [1.5;0.5;0.5;1.5];                      
+smoothTrajectory(v,waypoints,speed);          
 
 figure
 plot(waypoints(:,1),waypoints(:,2),'-o')
@@ -27,58 +29,58 @@ title('Vehicle Position Waypoints')
 
 
 
-%2)szenzorok definialasa
-%a) IMU
+
 mountingLocationIMU = [1 2 3];                                         
 mountingAnglesIMU = [0 0 0];                                            
-orientVeh2IMU = quaternion(mountingAnglesIMU,'eulerd','ZYX','frame'); 
+orientVeh2IMU = quaternion(mountingAnglesIMU,'eulerd','ZYX','frame');  
 imu = imuSensor('SampleRate',1/s.SampleTime,'ReferenceFrame','ENU');   
 
 
-%b) GPS
-mountingLocationGPS = [1 2 3];                                       
-mountingAnglesGPS = [50 40 30];                                      
+
+mountingLocationGPS = [1 2 3];                                        
+mountingAnglesGPS = [50 40 30];                                       
 orientVeh2GPS = quaternion(mountingAnglesGPS,'eulerd','ZYX','frame'); 
-gps = gpsSensor('ReferenceLocation',lla0,'ReferenceFrame','ENU');    
+gps = gpsSensor('ReferenceLocation',lla0,'ReferenceFrame','ENU');     
 
 
 
 
-
-accel = []; 
-gyro = [];  
-lla = [];   
-gpsVel = []; 
+accel = [];  
+gyro = [];   
+lla = [];    
+gpsVel = [];
 kalman_angles = [];  
-raw_angles_without_noise = [];
+raw_angles_without_noise = []; 
 raw_angles_with_noise = [];
 
-simSamplesPerGPS = (1/s.SampleTime)/gps.SampleRate; %GPS mintavételezési arány kiszámítása
+simSamplesPerGPS = (1/s.SampleTime)/gps.SampleRate; 
 idx = 0;
 
 
 
-simTime = 0; % Idő számláló
+
+simTime = 0;
 
 logData = [];
 idx = 0;
 
-dt = s.SampleTime;  % mintavételezési idő
+
+dt = s.SampleTime;  
 
 A = [1, dt; 0, 1];
 
 C = [1, 0];
 
-Q = [1e-5, 0; 0, 1e-3];  %1
+%Q = [1e-5, 0; 0, 1e-3];  %1
 %Q = [0.1, 0; 0, 0.2];    %2
 %Q = [0.01, 0; 0, 0.1];   %3
-%Q = [0.05, 0; 0, 0.05];  %4
+Q = [0.05, 0; 0, 0.05];  %4
   
-R = 1;     %1
+
+%R = 1;     %1
 %R = 0.001; %2
 %R = 0.1;   %3
-%R = 5;     %4
-
+R = 5;     %4
 
 
 xhat = [0; 0];  
@@ -92,23 +94,25 @@ filtered_angular_velocity = 0;
 while advance(s)
     groundTruth = state(v);                                                             
 
-                                                                                       
+                                                                                        
     posVeh = groundTruth.Position;                                                       
     orientVeh = quaternion(fliplr(groundTruth.Orientation), 'eulerd', 'ZYX', 'frame');   
-    velVeh = groundTruth.Velocity;                                                         
-    accVeh = groundTruth.Acceleration;                                                     
+    velVeh = groundTruth.Velocity;                                                          
+    accVeh = groundTruth.Acceleration;                                                      
     angvelVeh = deg2rad(groundTruth.AngularVelocity);                                         
     
    
     [posIMU,orientIMU,velIMU,accIMU,angvelIMU] = transformMotion( ...
         mountingLocationIMU,orientVeh2IMU, ...
-        posVeh,orientVeh,velVeh,accVeh,angvelVeh);                            
+        posVeh,orientVeh,velVeh,accVeh,angvelVeh);                             
     [accel(end+1,:), gyro(end+1,:)] = imu(accIMU,angvelIMU,orientIMU); 
     
-    
-    current_ax = accel(end,1);
-    current_ay = accel(end,2);
-    measured_angle = atan2(current_ay, current_ax) 
+    imuEul = eulerd(orientIMU, 'ZYX', 'frame'); 
+    imu_yaw = deg2rad(imuEul(1));
+    imu_pitch = deg2rad(imuEul(2));
+    imu_roll = deg2rad(imuEul(3));
+
+    measured_angle = imu_yaw %%ez a valos szog zaj nelkul
 
     raw_angles_without_noise(end+1) = measured_angle;
 
@@ -117,14 +121,17 @@ while advance(s)
 
     raw_angles_with_noise(end+1) = measured_angle_with_noise;
 
+    
     current_gz = gyro(end,3);
+    
+    
     
     % x̂⁻ₖ = A * x̂ₖ₋₁
     xhat_minus = A * xhat;
     
     % P⁻ₖ = A * Pₖ₋₁ * Aᵀ + Q
     P_minus = A * P * A' + Q;
-
+    
     
     % Kₖ = P⁻ₖ * Cᵀ * (C * P⁻ₖ * Cᵀ + R)⁻¹
     K = P_minus * C' / (C * P_minus * C' + R);
@@ -135,12 +142,13 @@ while advance(s)
     % Pₖ = (I - Kₖ * C) * P⁻ₖ
     P = (eye(size(K*C)) - K * C) * P_minus;
     
+    
     filtered_angle = xhat(1);
     filtered_angular_velocity = xhat(2);
     kalman_angles(end+1) = filtered_angle;
 
     
-
+  
     if (mod(idx, simSamplesPerGPS) == 0)
         [posGPS,orientGPS,velGPS,accGPS,angvelGPS] = transformMotion(...
             mountingLocationGPS, orientVeh2GPS,...
@@ -150,28 +158,28 @@ while advance(s)
         lastGPS = lla(end, :); % Frissítjük az utolsó ismert GPS pozíciót
     end
 
-                                                                                                                                                       
-    dataPoint = struct();
-    dataPoint.imuAccel = accel(end, :);                                                                                                               
-    dataPoint.imuGyro = gyro(end, :);                                                                                                                 
-                                                                                                                                                    
+                                                                                                                                                                                                 
+                                                                                                                                                     
     [currentLat, currentLon, currentAlt] = ned2geodetic(posGPS(1), posGPS(2), posGPS(3), lla0(1), lla0(2), lla0(3), referenceEllipsoid('wgs84'));
-    dataPoint.gpsPos = [currentLat, currentLon, currentAlt];                                                                                         
-                                                                                                                                                                                                                                                                                                         %plusz imu konstans 0-kal
-    dataPoint.imu2Accel = [-1, 0, 0];                                                                                                                     
-    dataPoint.imu2Gyro = [0, 0, 0];                                                                                                                    
+    
+    
+    dataPoint = struct();
 
-    jsonData = jsonencode(dataPoint);                                                                                                                      
+    dataPoint.gpsPos = [currentLat, currentLon, currentAlt];  
+    
+    dataPoint.imu2Angles = [imu_yaw, imu_roll, imu_pitch];
+    
+    dataPoint.imuAngles = [imu_yaw, imu_roll, imu_pitch];
 
-    if mod(idx,10) == 0
-        write(mqClient, topicToWrite, jsonData);
-    end
+    jsonData = jsonencode(dataPoint);
+
+    write(mqClient, topicToWrite, jsonData);
     
 
-    fprintf('IMU gyorsulás (m/s^2): [X: %.3f, Y: %.3f, Z: %.3f]\n', dataPoint.imuAccel(1), dataPoint.imuAccel(2), dataPoint.imuAccel(3));
-    fprintf('IMU giroszkóp (rad/s): [X: %.3f, Y: %.3f, Z: %.3f]\n', dataPoint.imuGyro(1), dataPoint.imuGyro(2), dataPoint.imuGyro(3));
-    fprintf('GPS pozíció (m): [X: %.6f, Y: %.6f, Z: %.6f]\n', dataPoint.gpsPos(1), dataPoint.gpsPos(2), dataPoint.gpsPos(3));
-    fprintf('Szög (rad) - Nyers: %.3f, Kalman: %.3f\n', measured_angle, filtered_angle);
+    
+    fprintf('GPS pozíció: [Lat: %.6f, Lon: %.6f, Alt: %.6f]\n', dataPoint.gpsPos(1), dataPoint.gpsPos(2), dataPoint.gpsPos(3));
+    fprintf('IMU szögek (rad): [Yaw: %.3f, Roll: %.3f, Pitch: %.3f]\n', dataPoint.imuAngles(1), dataPoint.imuAngles(2), dataPoint.imuAngles(3));
+    fprintf('Idő: %.2f s, Minta: %d\n', simTime, idx);
     fprintf('--------------------------------------------\n');
 
     idx = idx + 1;
@@ -192,6 +200,7 @@ max_error = max(abs_error);
 
 fprintf('Átlagos abszolút hiba: %.6f rad (%.2f fok)\n', mean_error, rad2deg(mean_error));
 fprintf('Maximális abszolút hiba: %.6f rad (%.2f fok)\n', max_error, rad2deg(max_error));
+
 
 
 figure
@@ -224,7 +233,7 @@ figure
 plot(1:length(raw_angles_with_noise), raw_angles_with_noise, 'b-', ...
      1:length(kalman_angles), kalman_angles, 'r-')
 
-legend('Nyers szög (atan2)', 'Kalman szűrt szög', 'FontSize', 18)
+legend('Nyers szög ', 'Kalman szűrt szög', 'FontSize', 18)
 title('Szög értékek összehasonlítása', 'FontSize', 20)
 xlabel('Minta', 'FontSize', 18)
 ylabel('Szög (rad)', 'FontSize', 18)
@@ -240,4 +249,4 @@ xlabel('Minta index')
 ylabel('Abszolút hiba (rad)')
 grid on
 
-%jelenlegi adatkuldes 100Hz(10ms)%mqtt inicializalas
+%jelenlegi adatkuldes 100Hz(10ms)
